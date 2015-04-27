d2a229801ed5842e4fdf9d3c92bbbd507c32e2d0
DavidTseng <ddd7975@gmail.com>
2015-04-27 17:58:11 +0800

    code review
    parallel computing into rma code

--- parallelComputing_test.R
+++ parallelComputing_test.R
@@ -1,13 +1,19 @@
 library(Rmpi)
 library(snow)
-
+temp <- 3
 mpi.spawn.Rslaves()
 system.time(mpi.parReplicate(100, mean(rnorm(1000000))))
 system.time(sapply(1:100, function(i)mean(rnorm(1000000))))
 mpi.close.Rslaves()
 
-
+temp <- 3
+temp2 <- 2
+temp3 <- c(1:100)
 cl <- makeCluster(4,type="SOCK")
-system.time(sapply(1:100,function(i) mean(rnorm(1000000))))
-system.time(parSapply(cl,1:100,function(i) mean(rnorm(1000000))))
+clusterExport(cl, list("temp", "temp2", "temp3"))
+#system.time(sapply(1:100,function(i) mean(rnorm(1000000))))
+system.time(parSapply(cl,1:100,function(i) {
+  temp*temp2*temp3[i]*mean(rnorm(1000000))
+  }))
 stopCluster(cl)
+
