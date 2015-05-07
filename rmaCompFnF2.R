71173e449243f6f28e37c56549206c969217fd6e
DavidTseng <ddd7975@gmail.com>
2015-05-07 17:59:58 +0800

    code review
    testing

--- rmaCompFnF2.R
+++ rmaCompFnF2.R
@@ -363,6 +363,7 @@ probMapping <- function(tpIni, tpEnd, index, ftab, uniTimePointWithZero){
   }
   return(prob)    
 }
+
 # making nList (need: datShipPro, YMD, )
 datShipPro <- dataM[[3]]
 uniqueProduct <- as.character(unique(datShipPro$Product_Name))
@@ -494,6 +495,11 @@ rmaNonparametric <- function(currentDate = currentDate, dataM, alpha = 0.05, min
         loc <- which(belongTime[l] == c(0, uniTimePoint))
         tmp[l] <- loc
         failureTable[loc, 2] <- failureTable[loc, 2] - 1
+      }
+      
+      if (min(failureTable[, 2]) < 0){
+        failureTable[1, 3] <- failureTable[1, 3] - sum(failureTable[which(failureTable[, 2] < 0), 2])
+        failureTable[which(failureTable[, 2] < 0), 2] <- 0
       }
       
       for (i in 2:(length(uniTimePoint) + 1)){
@@ -809,7 +815,7 @@ selectNi <- function(dataM, YMD, minNi = 5, currentDate){
 #---------------------------------------------------------------------------
 
 # input1: date
-currentDate <- "2005/04" # for simulation
+currentDate <- "2015/07" # for simulation
 ymd <- "2015/07"        # for last estimation
 # input2: component name
 componentName <- "1400000907"
@@ -845,7 +851,50 @@ tM1 <- proc.time()
 dataM <- dataArr(dat_all = dat_all, dat_com = dat_com, dat_shipping = dat_shipping, dat_future_shipping = dat_future_shipping, componentName = componentName, YMD = ymd)
 tM2 <- proc.time()
 tM2 - tM1
-elected <- selectNi(dataM = dataM, YMD = ymd, minNi = 20, currentDate = currentDate)
+datShipPro <- dataM[[3]]
+uniqueProduct <- as.character(unique(datShipPro$Product_Name))
+#
+minY <- dataM[[1]][1]; minM <- dataM[[1]][2]; minD <- dataM[[1]][3]
+dataComp_c <- dataM[[2]]
+datShipPro <- dataM[[3]]
+dat_censored1 <- dataM[[4]]
+n_break <- dataM[[5]]
+
+YMD <- ymd
+endMonth <- seq(as.Date(paste(c(YMD, "01"), collapse = "/")), length = 2, by = "months")[2]
+x1 <- as.character(seq(as.Date(paste(c(minY, minM, minD), collapse = "/")), 
+                       as.Date(endMonth), "months"))
+
+x <- as.character(sapply(x1, function(y){
+  tmp <- strsplit(y, "-")[[1]]
+  tmp[3] <- "01"
+  tmp2 <- paste(tmp[1], tmp[2], tmp[3], sep="/")
+  return(tmp2)
+}))
+
+# ----- split the x so that it can match the dat_shipping (because dat_shipping only record the amount by month)
+x_split <- 0
+for (i in 1:length(x)){
+  tmp <- strsplit(x[i], "/")[[1]]
+  x_split[i] <- paste0(tmp[1], tmp[2])
+}
+x_mid <- sapply(1:length(x), function(i){
+  tmpd <- strsplit(x[i], "/")[[1]]
+  tmpd[3] <- "15"
+  paste(tmpd, collapse = "/")
+})
+nList <- lapply(1:length(uniqueProduct), function(i){
+  datShipPro_i <- datShipPro[which(datShipPro$Product_Name == uniqueProduct[i]), ]
+  n_ship <- sapply(1:(length(x_split)), function(j){
+    return(max(sum(datShipPro_i[which(datShipPro_i$Shipping_DT == x_split[j]), "Qty"]), 0))
+  })
+  return(matrix(n_ship, nrow=1))
+})
+for (l in 1:length(nList)){
+  colnames(nList[[l]]) <- x_mid[1:(length(x_mid))]
+}
+names(nList) <- uniqueProduct
+elected <- selectNi(dataM = dataM, YMD = ymd, minNi = 5, currentDate = currentDate)
 t2 = proc.time()
 t2 - t1
 ##
